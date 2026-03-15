b57e52b8ee1cef594dec09046521c13e264880b8
Lohit-Surada <lohitsurada52@gmail.com>
2026-03-15 18:05:39 +0530

    utils.py is committed in soil app

--- backend/soil_app/utils.py
+++ backend/soil_app/utils.py
@@ -1,9 +1,10 @@
 import numpy as np
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 
 def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
+    img = Image.open(img_path).convert("RGB")
+    img = img.resize((224, 224))
+    img_array = np.asarray(img, dtype=np.float32)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0  # normalize to [0, 1] range expected by the model
     return img_array
