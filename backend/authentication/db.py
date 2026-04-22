7d4677646d4e545ca851458e9cfa33580a1843d9
Lohit-Surada <lohitsurada52@gmail.com>
2026-04-22 16:49:44 +0530

    changed

--- backend/authentication/db.py
+++
@@ -1,5 +0,0 @@
-from models.mongo import db
-
-users_collection = db["users"]
-admins_collection = db["admins"]
-recovery_collection = db["recovery"]
--- backend/authentication/validators.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def validate_user_id(user_id):
-    pattern = r"^23341A0[1-5][A-Z][0-9]$"
-    return re.match(pattern, user_id)
-
-def validate_admin_id(admin_id):
-    pattern = r"^ADM([0-2][0-9][0-9]|300|00[1-9]|0[1-9][0-9])$"
-    return re.match(pattern, admin_id)
